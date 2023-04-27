7e4e45186029d5196d766e3d77ee892bff643294
user <madhumathi.labglo@gmail.com>
2023-04-27 18:00:42 +0530

    change cors

--- index.js
+++ index.js
@@ -13,23 +13,25 @@ app.use(express.json())
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-var allowedOrigins = ['http://localhost:3001',
-                      'https://mernstack-frontend.vercel.app'];
+app.use(cors({ credentials: true, origin: ["http://localhost:3001","https://mernstack-frontend.vercel.app"] }))
+
+// const allowedOrigins = ['http://localhost:3001',
+//                       'https://mernstack-frontend.vercel.app'];
     
                        
-app.use(cors({
-  origin: function(origin, callback){
-    // allow requests with no origin 
-    // (like mobile apps or curl requests)
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
-      var msg = 'The CORS policy for this site does not ' +
-                'allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  }
-}));
+// app.use(cors({
+//   origin: function(origin, callback){
+//     // allow requests with no origin 
+//     // (like mobile apps or curl requests)
+//     if(!origin) return callback(null, true);
+//     if(allowedOrigins.indexOf(origin) === -1){
+//       var msg = 'The CORS policy for this site does not ' +
+//                 'allow access from the specified Origin.';
+//       return callback(new Error(msg), false);
+//     }
+//     return callback(null, true);
+//   }
+// }));
 
 const salt = bcrypt.genSaltSync(5);
 const secretkey = "hdjdfgkk485739dnf"
